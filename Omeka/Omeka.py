7bbd448a7a2e221cc68138f651ad8fac42fe5470
Diogo Francisco Machado Cosme <dfmce@iscte-iul.pt>
2023-07-21 15:01:36 +0100

    Added docstrings to Omeka REST API

--- Omeka/Omeka.py
+++ Omeka/Omeka.py
@@ -10,20 +10,23 @@ CREATOR_ELEMENT = ["Creator", 39]
 URL_ELEMENT = ["URL", 28]
 
 
-# it's necessary to add the element_set when the element is a URL
 def def_elementSet():
+    """it's necessary to add the element_set when the element is a URL"""
     return {'id': 3, 'url': 'https://resettingstt.omeka.net/api/element_sets/3', 'name': 'Item Type Metadata'}
 
 
 def def_itemType_hyperlink():
+    """"it's also necessary to add the item_type when the element is a URL"""
     return {'id': 11, 'url': 'https://resettingstt.omeka.net/api/item_types/11', 'name': 'Hyperlink', 'resource': 'item_types'}
 
 
 def def_element(typeOfElement):
+    """json format for the element name and for element id"""
     return {'id': typeOfElement[1], 'name': typeOfElement[0]}
 
 
 def add_element(element_text, typeOfElement):
+    """json format for element to be added to element_texts"""
     if typeOfElement[0] == "URL":
         dic = {'html': True, 'text': f"<a href={element_text}>{element_text}</a>",
                'element': def_element(typeOfElement),
@@ -34,6 +37,7 @@ def add_element(element_text, typeOfElement):
 
 
 def get_multipart_to_add_file(item_id, filename, file):
+    """multipart data to add a file to an item"""
     boundary = 'E19zNvXGzXaLvS5C'
     item = {"item": {"id": item_id}}
 
@@ -55,10 +59,12 @@ class Omeka:
         self.api_key = api_key
 
     def post(self, endpoint, post_data):
+        """Create a new item in Omeka"""
         print(post_data)
         print(requests.post(f"{self.url}/{endpoint}?key={self.api_key}", data=post_data))
 
     def post_file(self, multipart_data):
+        """Add a file to an Omeka item"""
         full_url = f"{self.url}/files?key={self.api_key}"
         response = requests.post(full_url, headers={'Content-Type': multipart_data.content_type}, data=multipart_data)
         print(f"{response.reason} \n {response.text}")
