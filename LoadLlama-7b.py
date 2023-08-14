c8d07c7837b99f030fb63a5a51b09b18baa13d36
Diogo Francisco Machado Cosme <dfmce@iscte-iul.pt>
2023-08-14 16:42:01 +0100

    LoadLlama file

--- LoadLlama-7b.py
+++ LoadLlama-7b.py
@@ -11,16 +11,20 @@ import subprocess, sys
 from transformers import AutoTokenizer, AutoModelForCausalLM
 
 tokenizer = AutoTokenizer.from_pretrained("Llama-2-7b-hf")
+print("Loaded tokenizer")
 model = AutoModelForCausalLM.from_pretrained("Llama-2-7b-hf")
+print("Loaded model")
 
 
 prompt = "Who is CR7?"
 
 # Tokenize the prompt
 input_ids = tokenizer.encode(prompt, return_tensors='pt')
+print("Loaded encode")
 
 # Generate text from the prompt
 output = model.generate(input_ids, temperature=0.0)
+print("Loaded output")
 
 # Decode the generated text
 generated_text = tokenizer.decode(output[0], skip_special_tokens=True)
