9c2d9300acba1f2c82093d995db2019e814a3ccb
Diogo Francisco Machado Cosme <dfmce@iscte-iul.pt>
2023-08-16 10:52:54 +0100

    LoadLlama file

--- LoadLlama.py
+++ LoadLlama.py
@@ -15,9 +15,9 @@ from transformers import AutoTokenizer, AutoModelForCausalLM
 
 login()
 
-tokenizer = AutoTokenizer.from_pretrained("meta-llama/Llama-2-13b-hf")
+tokenizer = AutoTokenizer.from_pretrained("meta-llama/Llama-2-13b-chat-hf")
 print("Loaded tokenizer")
-model = AutoModelForCausalLM.from_pretrained("meta-llama/Llama-2-13b-hf")
+model = AutoModelForCausalLM.from_pretrained("meta-llama/Llama-2-13b-chat-hf")
 print("Loaded model")
 
 
