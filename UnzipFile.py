2b518ad35a4a6e5a235e4e30126dd775d522ddfa
Diogo Francisco Machado Cosme <dfmce@iscte-iul.pt>
2023-08-23 12:46:39 +0100

    Unzip file

--- UnzipFile.py
+++ UnzipFile.py
@@ -1,4 +1,8 @@
-import zipfile
+import sys
+if sys.version_info >= (3, 6):
+    import zipfile
+else:
+    import zipfile36 as zipfile
 
 with zipfile.ZipFile('/home/dcosme/quivr-0.0.46.zip', 'r') as zip_ref:
     zip_ref.extractall('/home/dcosme')
