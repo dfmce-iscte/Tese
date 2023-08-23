c702045a27e60b7cde88ec153f163bac11977b09
Diogo Francisco Machado Cosme <100353807+dfmce-iscte@users.noreply.github.com>
2023-08-23 14:30:47 +0100

    Delete UnzipFile.py

--- UnzipFile.py
+++
@@ -1,9 +0,0 @@
-import sys
-
-if sys.version_info >= (3, 6):
-    import zipfile
-else:
-    import zipfile36 as zipfile
-
-with zipfile.ZipFile('quivr-0.0.46.zip', 'r') as zip_ref:
-    zip_ref.extractall("Quivr")
