1073ef996439962f6d26af229325c339f8da5aa4
Diogo Francisco Machado Cosme <dfmce@iscte-iul.pt>
2023-11-01 20:46:44 +0100

    ImportEUtoOmeka.ipynb

--- ExtractImagesFromPDF.py
+++ ExtractImagesFromPDF.py
@@ -1,55 +1,83 @@
 import os
 import fitz  # PyMuPDF library
+from PIL import Image
+
 from tqdm import tqdm  # tqdm library for progress tracking
 
 # Specify the directory containing PDF files
-workdir = "RandomisedSample"
+# workdir = "RandomisedSample"
+
 
 # Dictionary to store image counts per PDF
-image_counts = {}
-
-# Loop through each file in the directory
-for each_path in os.listdir(workdir):
-    if each_path.endswith(".pdf"):  # Check if the file is a PDF
-        pdf_name = each_path[:-4]  # Remove ".pdf" extension
-
-        # Create a directory for the PDF's images
-        pdf_image_dir = os.path.join(workdir, pdf_name)
-        os.makedirs(pdf_image_dir, exist_ok=True)
-
-        doc = fitz.Document(os.path.join(workdir, each_path))  # Open the PDF document
-
-        image_count = 0  # Counter for extracted images
-
-        for i in range(doc.page_count):
-            page = doc.load_page(i)
-            for image in page.get_images():
-                img = doc.extract_image(image[0])
-                image_ext = img["ext"]
-                image_data = img["image"]
-                with open(os.path.join(pdf_image_dir, "p%s-%s.jpeg" % (i, image[0])), 'wb') as f:
-                    f.write(image_data)
-                image_count += 1
-            break
+# image_counts = {}
 
 
-        # # Loop through each page of the PDF
-        # for i in tqdm(range(len(doc)), desc="pages"):
-        #     # Loop through each image on the page
-        #     for img in tqdm(doc.get_page_images(i), desc="page_images"):
-        #         xref = img[0]  # Get the image reference
-        #         image = doc.extract_image(xref)  # Extract the image data
-        #         pix = fitz.Pixmap(doc, xref)  # Create a pixmap from the image reference
-        #
-        #         # Save the pixmap as a PNG image within the PDF's directory
-        #         png_filename = os.path.join(pdf_image_dir, "p%s-%s.jpeg" % (i, xref))
-        #         pix.save(png_filename)
-        #
-        #         image_count += 1
-        #         pix = None  # Release the pixmap
+def convert_jpx_to_jpeg(jpx_path, jpeg_path):
+    with Image.open(jpx_path) as jpx_img:
+        # Convert and save as JPG
+        jpx_img.save(jpeg_path, 'JPEG')
+    os.remove(jpx_path)
 
-        image_counts[pdf_name] = image_count
 
-# Print the image counts for each PDF
-for pdf_name, count in image_counts.items():
-    print("Extracted %d pictures from %s.pdf" % (count, pdf_name))
+def get_pixels_width_height(img_path):
+    with Image.open(img_path) as img_open:
+        # Get the pixels
+        pixels = list(img_open.getdata())
+
+    return pixels
+
+
+def is_image_almost_black(pixels):
+    number_of_pixels = len(pixels)
+    black_pixeis = 0
+    for pixel in pixels:
+        r, g, b = pixel
+        if (r + g + b) / 765 < 0.1:
+            black_pixeis += 1
+
+    return black_pixeis / number_of_pixels > 0.55
+
+
+# Extract images from PDF and returns the directory containing the images
+def extract_images_from_pdf(pdf_path):
+    pdf_name = pdf_path.split("/")[2][:-4]  # Remove ".pdf" extension
+
+    # Create a directory for the PDF's images
+    pdf_image_dir = os.path.join(pdf_path[:-4])
+    # pdf_image_dir = workdir
+    os.makedirs(pdf_image_dir, exist_ok=True)
+
+    doc = fitz.Document(pdf_path)  # Open the PDF document
+
+    image_count = 0  # Counter for extracted images
+
+    for i in range(len(doc)):
+        for image in doc.get_page_images(i):
+            img = doc.extract_image(image[0])
+            image_ext = img["ext"]
+            image_data = img["image"]
+            image_path = os.path.join(pdf_image_dir, f"{pdf_name}_{image_count}.{image_ext}")
+            with open(image_path, 'wb') as f:
+                f.write(image_data)
+            image_count += 1
+
+            if image_ext == "jpx":
+                new_img_path = image_path.replace("jpx", "jpg")
+                convert_jpx_to_jpeg(image_path, new_img_path)
+                image_path = new_img_path
+
+            if is_image_almost_black(get_pixels_width_height(image_path)):
+                with open('images_to_fix.txt', 'a+') as f:
+                    f.write(image_path + "\n")
+
+    # print("Extracted %d pictures from %s.pdf" % (image_count, pdf_name))
+    return pdf_image_dir
+
+# # Loop through each file in the directory
+# for each_path in os.listdir(workdir):
+#     if each_path.endswith(".pdf"):  # Check if the file is a PDF
+#         extract_images_from_pdf(each_path)
+#
+# # Print the image counts for each PDF
+# for pdf_name, count in image_counts.items():
+#     print("Extracted %d pictures from %s.pdf" % (count, pdf_name))
--- Omeka/Omeka.py
+++ Omeka/Omeka.py
@@ -1,18 +1,36 @@
 import requests
 import json
 from requests_toolbelt.multipart.encoder import MultipartEncoder
+from geopy.geocoders import Nominatim
 
+geolocator = Nominatim(user_agent="omekaResettingGeoLoc")
+
+
+# melhorar isto para ir buscar o id do elemento a partir do nome
 # element name, element id
 TITLE_ELEMENT = ["Title", 50]
 SUBJECT_ELEMENT = ["Subject", 49]
 DESCRIPTION_ELEMENT = ["Description", 41]
 CREATOR_ELEMENT = ["Creator", 39]
-URL_ELEMENT = ["URL", 28]
+LOCAL_URL_ELEMENT = ["Local URL", 6]
 CONTRIBUTOR_ELEMENT = ["Contributor", 37]
+ADDRESS_ELEMENT = ["Address", 54]
+
+SEGITTUR_CATALOGUE = "Catalogue of Technological Solutions for Smart Destinations"
+SCOTTISH_CATALOGUE = "Technology solutions for tourism businesses in a post-Covid19 Scotland"
+EU_CATALOGUE_2022 = "Leading examples of Smart Tourism Practices in Europe (2022)"
+EU_CATALOGUE_2023 = "Leading examples of Smart Tourism Practices in Europe (2023)"
+
+
+stt_type = {
+    "id": 21,
+    "url": "https://sttobservatory.omeka.net/api/item_types/21",
+    "name": "STT",
+    "resource": "item_types"
+}
 
 
 def def_elementSet():
-    """it's necessary to add the element_set when the element is a URL"""
     return {'id': 3, 'url': 'https://resettingstt.omeka.net/api/element_sets/3', 'name': 'Item Type Metadata'}
 
 
@@ -27,14 +45,38 @@ def def_element(typeOfElement):
     return {'id': typeOfElement[1], 'name': typeOfElement[0]}
 
 
+# def add_element(element_text, typeOfElement):
+#     """json format for element to be added to element_texts"""
+#     if typeOfElement[0] == "URL":
+#         text = "<ul>"
+#         for url in element_text:
+#             text += f"<li><a href={url}>{url}</a></li>"
+#         text += "</ul>"
+#         # dic = {'html': True, 'text': f"<a href={element_text}>{element_text}</a>",
+#         dic = {'html': True, 'text': text,
+#                'element': def_element(typeOfElement),
+#                'element_set': def_elementSet()}
+#     else:
+#         dic = {'html': True, 'text': element_text, 'element': def_element(typeOfElement)}
+#     return dic
+
 def add_element(element_text, typeOfElement):
     """json format for element to be added to element_texts"""
-    if typeOfElement[0] == "URL":
-        dic = {'html': True, 'text': f"<a href={element_text}>{element_text}</a>",
-               'element': def_element(typeOfElement),
-               'element_set': def_elementSet()}
+    if typeOfElement[0] == LOCAL_URL_ELEMENT[0]:
+        if len(element_text) == 1:
+            text = f"<a href={element_text[0]}>{element_text[0]}</a>"
+        else:
+            text = "<ul>"
+            for url in element_text:
+                text += f"<li><a href={url}>{url}</a></li>"
+            text += "</ul>"
     else:
-        dic = {'html': False, 'text': element_text, 'element': def_element(typeOfElement)}
+        text = f"<p>{element_text}</p>"
+    dic = {'html': True,
+           'text': text,
+           'element': def_element(typeOfElement),
+           'element_set': def_elementSet()
+           }
     return dic
 
 
@@ -54,21 +96,46 @@ def get_multipart_to_add_file(item_id, filename, file):
     return multipart_data
 
 
-def create_json_item(title, description, creator, contributor, url, tags, collection=None):
+#
+# def create_json_item(title, description, urls, tags, creator=None, contributor=None, collection=None):
+#     data = {}
+#     element_texts = [add_element(title, TITLE_ELEMENT), add_element(description, DESCRIPTION_ELEMENT),
+#                      add_element(urls, URL_ELEMENT)]
+#
+#     if collection is not None:
+#         data["collection"] = {"id": collection}
+#     if creator is not None:
+#         element_texts.append(add_element(creator, CREATOR_ELEMENT))
+#     if contributor is not None:
+#         element_texts.append(add_element(contributor, CONTRIBUTOR_ELEMENT))
+#     data.update({
+#         "public": True,
+#         "item_type": stt_type,
+#         "tags": tags,
+#         "element_texts": element_texts
+#     })
+#     return json.dumps(data)
+
+def create_json_item(title, description, urls=None, tags=None, creator=None, collection=None, address=None):
+    if tags is None:
+        tags = []
     data = {}
+    element_texts = [add_element(title, TITLE_ELEMENT), add_element(description, DESCRIPTION_ELEMENT)]
+
     if collection is not None:
         data["collection"] = {"id": collection}
+    if urls is not None:
+        element_texts.append(add_element(urls, LOCAL_URL_ELEMENT))
+    if creator is not None:
+        element_texts.append(add_element(creator, CREATOR_ELEMENT))
+    if address is not None:
+        element_texts.append(add_element(address, ADDRESS_ELEMENT))
+
     data.update({
         "public": True,
-        "item_type": def_itemType_hyperlink(),
+        "item_type": stt_type,
         "tags": tags,
-        "element_texts": [
-            add_element(title, TITLE_ELEMENT),
-            add_element(description, DESCRIPTION_ELEMENT),
-            add_element(creator, CREATOR_ELEMENT),
-            add_element(contributor, CONTRIBUTOR_ELEMENT),
-            add_element(url, URL_ELEMENT)
-        ]
+        "element_texts": element_texts
     })
     return json.dumps(data)
 
@@ -83,10 +150,43 @@ class Omeka:
         """Create a new item in Omeka"""
         # print(post_data)
         response = requests.post(f"{self.url}/{endpoint}?key={self.api_key}", data=post_data)
-        print(f"{response}")
+        print(f"Post Item{response.reason}")
+        return json.loads(response.text)
 
     def post_file(self, multipart_data):
         """Add a file to an Omeka item"""
         full_url = f"{self.url}/files?key={self.api_key}"
         response = requests.post(full_url, headers={'Content-Type': multipart_data.content_type}, data=multipart_data)
-        print(f"{response.reason} \n {response.text}")
+        print(f"Post File to Item: {response.reason}")
+        return response
+
+    def get_collection_id(self, collection_name):
+        collections = json.loads(requests.get(f"{self.url}/collections").text)
+        for collection in collections:
+            for element_text in collection['element_texts']:
+                if element_text['element']['name'] == 'Title' and element_text['text'] == collection_name:
+                    return collection['id']
+        return -1
+
+    def get_geolocation_id(self, address):
+        locations = json.loads(requests.get(f"{self.url}/geolocations").text)
+        for geo in locations:
+            if geo["address"] == address:
+                return geo["id"]
+        return -1
+
+    def post_geolocation_if_doesnt_exist(self, address, item_id):
+        """Create a new geolocation in Omeka"""
+        location = geolocator.geocode(address)
+        data = {
+            "latitude": location.latitude,
+            "longitude": location.longitude,
+            "address": address,
+            "zoom_level": 15,
+            "map_type": "Leaflet",
+            "extended_resources": [],
+            "item": {"id": item_id}
+        }
+        response = requests.post(f"{self.url}/geolocations?key={self.api_key}", data=json.dumps(data))
+        print(f"Post Geolocation: {response.reason}")
+        return response, json.dumps(data)
